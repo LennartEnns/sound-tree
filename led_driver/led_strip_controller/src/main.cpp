056609a5e8814bfefccfd12aa45a8f96d8e7ff42
Thijs Boersma <thijs.boersma@sap.com>
2025-04-16 15:16:59 +0200

    speed up data transfer to mik

--- led_driver/led_strip_controller/src/main.cpp
+++ led_driver/led_strip_controller/src/main.cpp
@@ -10,7 +10,7 @@ void setup() {
     // initialize LED digital pin as an output.
     pinMode(LED_BUILTIN, OUTPUT);
 
-    Serial.begin(115200);
+    Serial.begin(1000000);
 
     pixels.begin();
 
@@ -21,31 +21,24 @@ int hex_byte_toint(char* digits) {
     return strtol(digits, NULL, 16);
 }
 void receiveDataAndShow() {
-    char charBuffer[6 * NUMPIXELS];
+    uint8_t charBuffer[3 * NUMPIXELS];
     int currentChar = 0;
-    while (currentChar < 6 * NUMPIXELS) {
+    while (currentChar < 3 * NUMPIXELS) {
         while (Serial.available() < 1) {} // wait for serial input
         charBuffer[currentChar] = Serial.read();
         currentChar++;
     } // buffer the right amount of chars
 
-    char tempData[3];
-    int nextData[3];
     for (int pixel = 0; pixel < NUMPIXELS; pixel++) {
-        for (int i = 0; i < 3; i++) {
-            tempData[0] = charBuffer[2 * i + pixel * 6];
-            tempData[1] = charBuffer[2 * i + 1 + pixel * 6];
-            tempData[2] = '\0'; // just a random invalid hex character, causes strtol to cancel
-        
-            nextData[i] = hex_byte_toint(tempData);
-        }
-        
-        pixels.setPixelColor(pixel, pixels.Color(nextData[0], nextData[1], nextData[2]));
+        pixels.setPixelColor(pixel, pixels.Color(
+            charBuffer[3 * pixel + 0],
+            charBuffer[3 * pixel + 1],
+            charBuffer[3 * pixel + 2])
+        );
     }
 
     pixels.show();
     //Serial.println("OK"); // messes up speed
-    
 }
 
 void loop() {
--- led_driver/led_strip_controller/test/test.py
+++ led_driver/led_strip_controller/test/test.py
@@ -2,9 +2,9 @@ from serial import Serial
 import time
 
 NUM_LEDS = 44
-FPS = 30
+FPS = 300
 
-target_colors = ['FF00FF', '00FFFF', 'FFFF00']
+target_colors = [[b'\xFF',b'\x00',b'\x00'], [b'\x00',b'\xFF',b'\x00'], [b'\x00',b'\x00',b'\xFF']]
 def new_test_array(startColor):
     test_array = []
     ci = startColor
@@ -13,26 +13,39 @@ def new_test_array(startColor):
         ci = (ci + 1) % 3
     return test_array
 
-ser = Serial("/dev/cu.usbserial-1110", 115200)
+ser = Serial("/dev/cu.usbserial-1110", 1000000)
 
 def send_all(color_array): # len(color_array) = NUM_LEDS, contains unicode strings of length 6 (hex color value)
     for i in range(NUM_LEDS):
-        for j in range(6):
+        for j in range(3):
             sendChar = color_array[i][j]
-            ser.write(sendChar.encode())
-
+            ser.write(sendChar)
+ 
 # Read line (wait for ready)
-bs = ser.readline()
-print(bs)
+print(ser.readline())
 
 test_array = new_test_array(0)
 
+print("start sending")
 cnt = 0
-while True:
-    # wait and send new array
-    time.sleep(1.0/FPS)
-    send_all(test_array)
 
-    # populate array with new data
-    cnt = (cnt + 1) % 3
-    test_array = new_test_array(cnt)
+avgfps = 0
+frame = 0
+try:
+    while True:
+        start = time.time()
+        # wait and send new array
+        time.sleep(1.0/FPS)
+
+        send_all(test_array)
+
+
+        # populate array with new data
+        cnt = (cnt + 1) % 3
+        test_array = new_test_array(cnt)
+        end = time.time()
+
+        avgfps = (avgfps * frame + (end - start)) / (frame + 1)
+        frame += 1
+except KeyboardInterrupt:
+    print(1 / avgfps)
