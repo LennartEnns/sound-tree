d15e823d779867ca91eb5042616d60dc63033e31
Nico Noeller <nico.noeller@sap.com>
2025-04-17 13:05:33 +0200

    Add webinterface

---
+++ webinterface/js/receiver.js
@@ -0,0 +1,14 @@
+function startReceiver(){
+    // Create WebSocket connection.
+    const socket = new WebSocket("ws://localhost:8080");    
+
+    // Connection opened
+    socket.addEventListener("open", (event) => {
+    
+    }); 
+
+    // Listen for messages
+    socket.addEventListener("message", (event) => {
+      console.log("Message from server ", event.data);
+    });
+}
